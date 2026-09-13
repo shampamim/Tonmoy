708afc93d0d71cdb0d983e001c7dcde6f0bda859
Shampa Mim <328099991+shampamim@users.noreply.github.com>
2026-09-13 11:15:11 +0600

    abul

---
+++ basic.py
@@ -0,0 +1,21 @@
+print("Hello World")
+ 
+#variable sob kiso store kore rkha
+name=2009
+print(name)
+
+mim=500
+print(mim)
+
+
+#int date type 
+m=23
+print(m)
+
+#floating type 
+t=30.82
+print(t)
+
+#complex type 
+k=50j
+print(k)
---
+++ operators.py
@@ -0,0 +1,15 @@
+a = int(input("enter a number: "))
+b =  int (input("enter another number: "))
+s = a+b
+m = a*b 
+
+print(f"the sum : {s}" )
+
+print(f'the multi: {m}')
+
+d = a/b
+print(f"the division: {d}")
+r = a%b
+print(f"the reminder:{r}")
+p = a**b
+print(f"the power:{p}")
